f1d4d28b97e8b10be83a1d2d8dcefaa46ebb0e03
shin-tran <ngoc.shintrann@gmail.com>
2025-11-25 19:33:29 +0700

    feat: Update AssetRipper URL to the latest version for improved asset extraction

--- src/config.py
+++ src/config.py
@@ -7,7 +7,7 @@ APK_REGEX = re.compile(
     r"https://apk\.chillyroom\.com/apks/[\w\d.\-]+/SoulKnight-release-chillyroom-([\w\d.\-]+)\.apk"
 )
 
-ASSET_RIPPER_URL = "https://github.com/AssetRipper/AssetRipper/releases/download/1.0.106/AssetRipper_linux_x64.zip"
+ASSET_RIPPER_URL = "https://github.com/AssetRipper/AssetRipper/releases/download/1.3.6/AssetRipper_linux_x64.zip"
 
 LANGUAGES: List[str] = [
     "English",
